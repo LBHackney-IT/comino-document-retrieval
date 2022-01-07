50cf9a09f955082eefa6723f5f4ec76672c4aab8
Pedro <pedro.soares@madetech.com>
2022-01-07 11:03:59 +0000

    Add code to Convert Document to correct type

--- api/lib/UseCases/GetPreSignedUrl.js
+++ api/lib/UseCases/GetPreSignedUrl.js
@@ -1,7 +1,14 @@
+const { MimeType, W2DocType, W2DocExtensionLookup } = require('../Constants');
+const mimeTypes = require('mime-types');
+
+
 module.exports = (options) => {
   const s3Gateway = options.s3Gateway;
-  return async (id) => {
-    const url = await s3Gateway.getUrl(id, "msg", "xml");
+  return async (metadata) => {
+    const fileExt = W2DocExtensionLookup[metadata.type] || metadata.extension;
+    const mimeType = mimeTypes.lookup(fileExt) || MimeType.Default;
+    const id = metadata.imageId;
+    const url = await s3Gateway.getUrl(id, mimeType, fileExt);
     return url;
   };
 };
